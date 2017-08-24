956e91f1800cc92f8ee177e194532b0669a2c15d
kaspersky518 <kaspersky518@gmail.com>
2017-08-24 16:50:51 +0800

    temporary

--- mabitd.py
+++ mabitd.py
@@ -1,8 +1,43 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from hmodel import *
 
 def pathbyname(name, ext):
     return '{folder}/{file}.{filetype}'.format(folder = 'img', file = name, filetype = ext)
+
+@app.route('/_add_numbers')
+def add_numbers():
+    """Add two numbers server side, ridiculous but well..."""
+    data_products = products.query.order_by(products.id).all()
+
+    productdict = dict()
+    productlist = list()
+    for data in data_products:
+        productdict[data.name] = [pathbyname(data.picname, data.picext), data.external, pathbyname(data.psqname, data.psqext)]
+        productlist.append(data.name)
+        print(data.name)
+
+    print(productdict)
+    print(productlist)
+    a = request.args.get('a', 0, type=int)
+    resultpic = productdict[productlist[a]][0]
+    return jsonify(result=resultpic)
+
+@app.route('/products', methods=['GET'])
+def productpage():
+
+    data_products = products.query.order_by(products.id).all()
+
+    productdict = dict()
+    productlist = list()
+    for data in data_products:
+        productdict[data.name] = [pathbyname(data.picname, data.picext), data.external, pathbyname(data.psqname, data.psqext)]
+        productlist.append(data.name)
+        print(data.name)
+
+    print(productdict)
+    print(productlist)
+    return render_template('products.html', productdict = productdict, productlist = productlist)
+
 
 @app.route('/', methods=['GET'])
 def index():

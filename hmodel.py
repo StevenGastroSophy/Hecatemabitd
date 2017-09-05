019d16aa892495b59ebd07d39270adf65cd53377
kaspersky518 <kaspersky518@gmail.com>
2017-09-06 01:43:46 +0800

    修改資料表類型

--- hmodel.py
+++ hmodel.py
@@ -5,24 +5,24 @@ db = SQLAlchemy()
 class slidepics(db.Model):
     __tablename__ = 'slidepics'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256))
-    ext = db.Column(db.String(64))
+    name = db.Column(db.String)
+    ext = db.Column(db.String)
 
 
 class products(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(256))
-    code = db.Column(db.String(256))
-    description = db.Column(db.String(256))
-    picname = db.Column(db.String(256))
-    picext = db.Column(db.String(64))
-    psqname = db.Column(db.String(256))
-    psqext = db.Column(db.String(64))
+    name = db.Column(db.String)
+    code = db.Column(db.String)
+    description = db.Column(db.String)
+    picname = db.Column(db.String)
+    picext = db.Column(db.String)
+    psqname = db.Column(db.String)
+    psqext = db.Column(db.String)
     price = db.Column(db.Integer)
 
 class hecatestatus(db.Model):
     __tablename__ = 'hecatestatus'
-    id = db.Column(db.String(256), primary_key=True)
-    status = db.Column(db.String(256))
-    channel = db.Column(db.String(256))
+    id = db.Column(db.String, primary_key=True)
+    status = db.Column(db.String)
+    channel = db.Column(db.String)

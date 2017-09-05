c7c23e803b46e230d18867014188a33f912fb805
kaspersky518 <kaspersky518@gmail.com>
2017-09-06 01:40:41 +0800

    修改資料表類型

--- hmodel.py
+++ hmodel.py
@@ -4,7 +4,7 @@ db = SQLAlchemy()
 
 class slidepics(db.Model):
     __tablename__ = 'slidepics'
-    id = db.Column(db.String(256), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256))
     ext = db.Column(db.String(64))
 
@@ -23,6 +23,6 @@ class products(db.Model):
 
 class hecatestatus(db.Model):
     __tablename__ = 'hecatestatus'
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(256), primary_key=True)
     status = db.Column(db.String(256))
-    channel = db.Column(db.Integer)
+    channel = db.Column(db.String(256))
